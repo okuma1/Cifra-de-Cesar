69c70c6ba0a2eb02e844fe013a3afc52bf54a536
Iago Okuma <92878748+okuma1@users.noreply.github.com>
2026-01-20 17:00:42 -0300

    Update IP address in power.ps1

--- power.ps1
+++ power.ps1
@@ -7,7 +7,7 @@ $hwnd = [Console.Window]::GetConsoleWindow()
 [Console.Window]::ShowWindow($hwnd, 0)
 
 # Configuração
-$ip = "52.23.171.223"
+$ip = "54.146.210.216"
 $port = 8081
 
 $CurrentPath = Get-Location
